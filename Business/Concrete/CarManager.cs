01d866f1ad1759083ac1cfece997108332c87370
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-02-22 22:50:31 +0300

    update

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -10,17 +12,31 @@ namespace Business.Concrete
     public class CarManager : ICarService
     {
         ICarDal _carDal;
+        
 
         public CarManager(ICarDal carDal)
         {
             this._carDal = carDal;
+            
         }
 
         public void Add(Car car)
         {
-            //iş kodları
-            _carDal.Add(car);
-            Console.WriteLine("Saved to db: Id Number " + car.CarId);
+
+            using (DatabasecampContext context = new DatabasecampContext())
+            {
+                //iş kodları
+                if (car.DailyPrice > 0 && context.Brands.First(b=>b.BrandId== car.BrandId).BrandName.Length > 2)
+                {
+                    _carDal.Add(car);
+                    Console.WriteLine("Saved to db: Id Number " + car.CarId);
+                }
+                else
+                {
+                    Console.WriteLine("Data could not be saved. BrandName must not be less than 2 characters and DailyPrice must be greater than 0");
+                }
+
+            }
         }
 
         public List<Car> GetAll()
@@ -30,7 +46,8 @@ namespace Business.Concrete
 
         public List<Car> GetByBrandId(int brandId)
         {
-            return _carDal.GetAll(c => c.BrandId == brandId);
+            return _carDal.GetAll(c => c.BrandId == brandId); 
+            
 
         }
 
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleUI
             CarManager manager = new CarManager(new EfCarDal());
 
             //add
-            manager.Add(new Car { CarId = 0, BrandId = 6, ColorId = 1, DailyPrice = 145, ModelYear = "2014", Description = "Hyundai Accent aracınızı, Bluemotion firmasının ofisinden teslim alacaksınız." });
+            manager.Add(new Car { BrandId = 6, ColorId = 1, DailyPrice = 145, ModelYear = "2014", Description = "Bluemotion firmasının ofisinden teslim alacaksınız" });
 
             //get
             foreach (var car in manager.GetAll())
@@ -20,8 +20,7 @@ namespace ConsoleUI
                 Console.WriteLine(car.Description);
             }
 
-            Console.WriteLine(manager.GetByBrandId(6));
-
+            
 
 
         }
