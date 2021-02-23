13f019f563c61a5a7e5c50d988428b94d4572c62
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-02-23 12:56:03 +0300

    update

---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        IColorDal _colorDal;
+
+        public ColorManager(IColorDal colorDal)
+        {
+            _colorDal = colorDal;
+        }
+
+        public List<Color> GetAll()
+        {
+            return _colorDal.GetAll();
+        }
+        
+        //select * from Categories where CategoryId = 3
+        public Color GetById(int colorId)
+        {
+            return _colorDal.Get(c => c.ColorId == colorId);
+        }
+    }
+}
