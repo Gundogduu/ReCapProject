657da83c2ff959f46f758529049f613cbff34433
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-02-26 18:18:02 +0300

    update

--- Entities/DTOs/UserDetailDto.cs
+++ Entities/DTOs/UserDetailDto.cs
@@ -7,7 +7,6 @@ namespace Entities.DTOs
 {
     public class UserDetailDto:IDto
     {
-        public int UserDTOId { get; set; }
         public int UserId { get; set; }
         public int CustomerId { get; set; }
         public string CompanyName { get; set; }
