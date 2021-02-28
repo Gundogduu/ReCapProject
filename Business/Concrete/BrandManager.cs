e0db46fd01b4ec0de70f8a5a3ad673093e7ddba2
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-02-28 23:11:14 +0300

    update

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -13,32 +16,41 @@ namespace Business.Concrete
 
         public BrandManager(IBrandDal brandDal)
         {
-            this._brandDal = brandDal;
+            _brandDal = brandDal;
         }
 
-        public void Add(Brand brand)
+        public IResult Add(Brand brand)
         {
-            throw new NotImplementedException();
+
+            using (DatabasecampContext context = new DatabasecampContext())
+            {
+                _brandDal.Add(brand);
+                return new SuccessResult(Messages.BrandAdded);
+            }
         }
 
-        public void Delete(Brand brand)
+        public IResult Delete(Brand brand)
         {
-            throw new NotImplementedException();
+            _brandDal.Delete(brand);
+
+            return new SuccessResult(Messages.BrandDeleted);
         }
 
-        public List<Brand> GetAll()
+        public IDataResult<List<Brand>> GetAll()
         {
-            return _brandDal.GetAll();
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(),Messages.BrandsListed);
         }
 
-        public Brand GetById(int brandId)
+        public IDataResult<Brand> GetById(int brandId)
         {
-            return _brandDal.Get(b => b.BrandId == brandId);
+           return new SuccessDataResult<Brand>(_brandDal.Get(b => b.BrandId == brandId));
         }
 
-        public void Update(Brand brand)
+        public IResult Update(Brand brand)
         {
-            throw new NotImplementedException();
+            _brandDal.Delete(brand);
+
+            return new SuccessResult(Messages.BrandUpdated);
         }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -9,8 +9,8 @@ namespace Business.Constants
         public static string CarAdded = "Araba eklendi";
         public static string CarInValid = "Araba ismi geçersiz";
         public static string CarsListed = "Arabalar listelendi";
-        public static string CarUpdated = "Veri güncellendi";
-        public static string CarDeleted = "Veri silindi";
+        public static string CarUpdated = "Araba güncellendi";
+        public static string CarDeleted = "Araba silindi";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string CarRented = "Araba kiralandı";
         public static string RentalsListed = "Kiralamalar listelendi";
@@ -24,7 +24,15 @@ namespace Business.Constants
         public static string UsersListed = "Kullanıcılar listelendi";
         public static string UserUpdated = "Kullanıcı güncellendi";
         public static string UserDeleted = "Kullanıcı silindi";
-
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka güncellendi";
+        public static string BrandsListed = "Markalar listelendi";
+        public static string ColorAdded = "Renk eklendi";
+        public static string ColorUpdated = "Renk güncellendi";
+        public static string ColorDeleted = "Renk silindi";
+        public static string ColorsListed = "Renkler listelendi";
+        
     }
 }
 
